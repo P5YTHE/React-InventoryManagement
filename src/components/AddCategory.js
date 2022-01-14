565f6f74a871f5b29b3601a53bd0aa43a8c82458
Dishank Machkar <dishankdm@gmail.com>
2022-01-14 13:16:50 +0530

    Pagination

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -3,10 +3,61 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button} from "@material-ui/core";
 import { useNavigate } from "react-router";
+import axios, { Axios } from 'axios';
+import {useState} from 'react';
+import Auth from "../Auth/Auth"
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function AddCategory() {
+const useStyles = makeStyles((theme) => ({
+    form : {
+    padding:10,
+  },
+}));
+
+function AddCategory() {
+
+    const classes = useStyles();
+
     const navigate = useNavigate();
+
+    const categoryUrl='https://localhost:7157/api/Categories';
+    const[categoryData,setCategoryData]=useState({
+      categoryName: "",
+    });
+
+    function handle(e){
+      const newData={...categoryData}
+      newData[e.target.id]=e.target.value
+      setCategoryData(newData)
+      console.log(newData)
+    }
+
+    function handleSubmit(e){
+      e.preventDefault();
+      axios.post(categoryUrl,{
+        categoryName: categoryData.categoryName,
+        
+      },{
+        headers: {
+          Authorization: `Bearer ${Auth.getAccessToken()}`,
+        }
+      } 
+      )
+      .then(res=>{
+        console.log(res.categoryData)
+      }).catch(err=>{
+        console.log(err)
+      })
+    }
+
   return (
+    <>
+    <div className={classes.Box}>
+      <div>
+        <h3>Add Category</h3>
+      </div>
+      
+    <form onSubmit={(e) => handleSubmit(e)}>
     <Box
     component="form"
     sx={{
@@ -17,27 +68,23 @@ export default function AddCategory() {
   >
     <div>
       <TextField
+        onChange={(e) => handle(e)}
         required
         id="outlined-required"
         label="Required"
-        defaultValue="Category Name"
-      />     
-    </div>
-    {/* <div>
-    <TextField
-        disabled
-        id="outlined-disabled"
-        label="CategoryId"
-        defaultValue="CategoryID"
+        //defaultValue="Category Name"
       />
-      
-    </div> */}
+    </div>
+    </Box>
+    <br/>
     <div>
-    <Button size="small" variant="contained" color="inherit" onClick={()=>{navigate("/categories")}}>Save</Button>
+    <Button type="submit" size="small" variant="contained" color="inherit" onClick={()=>{navigate("/categories")}}>Save</Button>
     </div>
     
-  </Box>
+    </form>
+  </div>
+  </>
   );
 }
 
-
+export default AddCategory;
--- src/components/ViewCategoryProducts.js
+++ src/components/ViewCategoryProducts.js
@@ -1,36 +1,29 @@
-import {Box, Divider,List, ListItem, CircularProgress } from "@material-ui/core";
+import { Button, Box, Divider, List, ListItem, CircularProgress } from "@material-ui/core";
 import ProductCard from "./ProductCard";
 import {Grid} from "@material-ui/core";
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 import { getAuthorizationHeader } from '../utilities';
 import { makeStyles } from "@material-ui/core/styles";
+import ReactPaginate from 'react-paginate';
+import { display } from "@mui/system";
 
 const ViewCategoryProducts = (props) => {
     const url='https://localhost:7075/api/Products';
-    const[products,setProduct]= useState([]);    
-    const[loading,setLoading]=useState(false); 
+    const[products,setProduct]= useState([]);
+    const[searchTerm,setSearchTerm]=useState('');
+    const[loading,setLoading]=useState(false);
+    const[pageNumber,setPageNumber]=useState(0);
+    const productsPerPage=1;
+    const pagesVisited=pageNumber*productsPerPage;
     
-    const useStyles = makeStyles((theme) => ({
-            searchbox:{
-                outline: "1px",
-                width: "50vw",
-                border: 1,
-                float: "left",
-                padding: "8px",
-                background: "none",
-                boxShadow:"0 2px 2px #379bff"
-            }
-        }));
-    const classes = useStyles();
 
     useEffect(()=>{
         axios.get(url,getAuthorizationHeader()).then(response=>{
             setProduct(response.data);            
         })
         setLoading(true);
-    },[url]);  
-      
+    },[url]); 
 
     const filteredProducts = products.filter((product)=>{
         if(product.categoryId==props.categoryId)
@@ -38,42 +31,84 @@ const ViewCategoryProducts = (props) => {
             return product;
         }        
     })    
+
+    const displayProducts = filteredProducts
+                .slice(pagesVisited,pagesVisited+productsPerPage)
+                .map((product)=>{
+                    return (
+                        <>
+                            <Grid item key={product.productId}>
+                                <ProductCard
+                                    imageUrl1 = {product.imageUrl1} 
+                                    productName={product.productName}
+                                    productTag = {product.productTag}
+                                    productDesc = {product.productDesc}
+                                    productDiscount ={product.productDiscount}
+                                    productPrice = {product.productPrice} 
+                                    productId = {product.productId}
+                                    productObj = {product}
+                                    />
+                                </Grid>
+                        </>
+                    );
+                });
+
+
+
+const pageCount = Math.ceil(filteredProducts.length/productsPerPage);   
+
+
+const changePage = ({selected}) => {    
+    setPageNumber(selected);
+}
+
+    const useStyles = makeStyles((theme) => ({
+        searchbox:{
+            outline: "1px",
+            width: "50vw",
+            border: 1,
+            float: "left",
+            padding: "8px",
+            background: "none",
+            boxShadow:"0 2px 2px #379bff",
+        }  
+      }));
+
+    const classes = useStyles(); 
          
     console.log(products);    
     
     return(
-        <>
-            <List>                
-                <ListItem>
-                <Divider/>             
+        <>        
+            <List>
+                
+                <ListItem>                
                     <Grid container
                         direction="row"
                         justifyContent="space-evenly"
                         alignItems="center"
                         spacing={4}
                     >             
-                    {loading?(<></>):(<CircularProgress/>)}            
-                    {filteredProducts.map(product =>(
-                        <Grid item key={product.productId}>
-                            <ProductCard
-                                imageUrl1 = {product.imageUrl1} 
-                                productName={product.productName}
-                                productTag = {product.productTag}
-                                productDesc = {product.productDesc}
-                                productDiscount ={product.productDiscount}
-                                productPrice = {product.productPrice}     
-                                productObj = {product}                  
-                                />
-                        </Grid>
-                    ))}                  
+                        {displayProducts}                    
                     </Grid>            
                 </ListItem>
-            </List>            
+                <ListItem>
+                    <ReactPaginate
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        containerClassName={"paginationButton"}
+                        previousLinkClassName={"previousLink"}
+                        nextLinkClassName={"nextLink"}
+                        disabledClassName={"paginationDisable"}
+                        activeClassName={"paginationActive"}
+                    />
+                </ListItem>
+            </List>        
         </>
     )
 };
-
-
 
 
 export default ViewCategoryProducts;
