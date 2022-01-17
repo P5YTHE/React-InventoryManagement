5e96365c3920007654ae4f2ada3e1f8ef27b98a1
Komal Patil <komap153@gmail.com>
2022-01-17 13:43:26 +0530

    steps added

--- src/components/AddProductDesign.js
+++ src/components/AddProductDesign.js
@@ -13,6 +13,16 @@ import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddProduct2 from './AddProduct2';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import DescriptionIcon from '@mui/icons-material/Description';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import CreateIcon from '@mui/icons-material/Create';
 
 
 const steps = ['Add Product', 'Add Size'];
@@ -22,7 +32,7 @@ function getStepContent(step) {
     case 0:
       return <AddProduct2 />;
     case 1:
-      return(<React.Fragment>
+      return (<React.Fragment>
         <Typography variant="h5" gutterBottom>
           Thank you for your time.
         </Typography>
@@ -38,6 +48,20 @@ function getStepContent(step) {
 const theme = createTheme();
 
 export default function Checkout() {
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="right" ref={ref} {...props} />;
+  });
+  
+    const [open, setOpen] = React.useState(false);
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
   const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
@@ -50,7 +74,73 @@ export default function Checkout() {
 
   return (
     <ThemeProvider theme={theme}>
+      <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        <DescriptionIcon fontSize="small"/> Steps
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Steps to add the product"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+          <Typography>
+          <Typography variant="p" style={{color:"#444444"}}><CreateIcon fontSize="small"/>Add Product</Typography>
+          <p style={{color:"#585858"}}>1.Fill the details <br/>
+             2.Upload product pictures<br/>
+             3.Click on Add Product button<br/>
+             4.If product has more sizes select checkbox and follow the steps below</p>
+          <Typography variant="p" style={{color:"#444444"}}><CreateIcon fontSize="small"/>Add Size</Typography>
+          <p style={{color:"#585858"}}>1.Follw the steps above to Add Product first and click Add Product<br/>
+             2.Size details form will appear<br/>
+             3.Fill size details<br/>
+             4.Click Add Size<br/>
+             5.Click Confirm to finish
+          </p>
+          </Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+      {/* <Box
+        sx={{
+          display: 'flex',
+          '& > :not(style)': {
+            width: "48vh",
+            height: "70vh",
+            float: "left"
+          },
+        }}
+      >
+        <Paper variant="outlined" elevation={3}>
+          <Typography variant="h6" gutterBottom style={{ color: "#1976D2" }}>
+            <DescriptionIcon fontSize="small"/> Steps:
+          </Typography>
+          <Typography>
+          <Typography variant="p" style={{color:"#444444"}}><FiberManualRecordIcon fontSize="small"/>Add Product</Typography>
+          <p style={{color:"#585858"}}>1.Fill the details <br/>
+             2.Upload product pictures<br/>
+             3.Click on Add Product button<br/>
+             4.If product has more sizes select checkbox and follow the steps below</p>
+          <Typography variant="p" style={{color:"#444444"}}><FiberManualRecordIcon fontSize="small"/>Add Size</Typography>
+          <p style={{color:"#585858"}}>1.Follw the steps above to Add Product first and click Add Product<br/>
+             2.Size details form will appear<br/>
+             3.Fill size details<br/>
+             4.Click Add Size<br/>
+             5.Click Confirm to finish
+          </p>
+          </Typography>
+        </Paper>
+      </Box> */}
       <CssBaseline />
+
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
@@ -88,7 +178,7 @@ export default function Checkout() {
                     onClick={handleNext}
                     sx={{ mt: 3, ml: 1 }}
                   >
-                    {activeStep === steps.length ? 'Confirm' : 'Confirm'}
+                    {activeStep === steps.length ? '' : 'Confirm'}
                   </Button>
                 </Box>
               </React.Fragment>
